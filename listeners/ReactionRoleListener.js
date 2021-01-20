c21bd7939498345e6ba3708ed883a8b8268f691f
destial <destialpvp@gmail.com>
2021-01-20 13:15:07 +0800

    experimental emoji on setup

--- listeners/ReactionRoleListener.js
+++ listeners/ReactionRoleListener.js
@@ -78,18 +78,9 @@ module.exports = {
         client.on('emojiDelete', async (emoji) => {
             const server = await servers.fetch(emoji.guild.id);
             if (server) {
-                const reactionPanel = server.getReactionRoleManager().fetch(reaction.message.id);
-                if (reactionPanel) {
-                    const reactionrole = reactionPanel.getReactionRole(reaction.emoji.id);
-                    if (reactionrole) {
-                        const member = server.guild.members.cache.get(user.id);
-                        if (member) {
-                            if (member.roles.cache.get(reactionrole.role.id)) {
-                                member.roles.remove(reactionrole.role);
-                            }
-                        }
-                    }
-                }
+                server.getReactionRoleManager().panels.forEach(panel => {
+                    panel.removeReactionRoleByEmoji(emoji.id);
+                });
             }
         });
     }
